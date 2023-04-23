4a3b8f4419249b24aa25fde896b1aeb882da3b57
futa4095 <kick20000@gmail.com>
2023-04-23 15:55:38 +0900

    tokenの検証を追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
+require 'jwt'
+
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
 
-  # private
+  private
 
   def authenticate
-    logger.info request.headers[:HTTP_AUTHORIZATION]
+    hmac_secret = ENV.fetch('SUPABASE_JWT_SECRET', nil)
     authenticate_or_request_with_http_token do |token, _options|
-      logger.info token
+      decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+      logger.info decoded_token[0]['sub']
+      logger.info decoded_token
       true
     end
   end
