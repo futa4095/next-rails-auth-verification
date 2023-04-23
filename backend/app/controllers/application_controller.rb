f8c550753e8f7c4536bfb6498403c00737829c85
futa4095 <kick20000@gmail.com>
2023-04-23 20:11:58 +0900

    常にauthenticateがtrueになっていたのを修正(ただし不十分)

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -12,9 +12,13 @@ class ApplicationController < ActionController::API
     hmac_secret = ENV.fetch('SUPABASE_JWT_SECRET', nil)
     authenticate_or_request_with_http_token do |token, _options|
       decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
-      logger.info decoded_token[0]['sub']
-      logger.info decoded_token
-      true
+      fetch_current_user decoded_token
     end
+  end
+
+  def fetch_current_user(decoded_token)
+    logger.info decoded_token[0]['sub']
+    logger.info decoded_token
+    @curernt_user = true
   end
 end
